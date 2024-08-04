f819b35ed98c151613449f86ce7ca9d2c872e7eb
PRAVEEN887 <praveendhamodharncs2020@gmail.com>
2024-08-04 22:11:42 +0530

    .

--- Patterns/Test10.java
+++ Patterns/Test10.java
@@ -2,6 +2,19 @@ package Patterns;
 
 public class Test10 {
     public static void main(String[] args) {
-        
+        int n=5;
+        for(int i=0;i<=2*n-1;i++){
+            int stars=i;
+            if(stars>n){
+                stars=2*n-i;
+            }
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+
+        }
+
     }
 }
