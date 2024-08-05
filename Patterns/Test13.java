08ab8aa6934e177f429d2bdff541ce1ab765416d
PRAVEEN887 <praveendhamodharncs2020@gmail.com>
2024-08-05 22:14:34 +0530

    .

--- Patterns/Test13.java
+++ Patterns/Test13.java
@@ -2,18 +2,18 @@ package Patterns;
 
 public class Test13 {
     public static void main(String[] args) {
-        //symmetry
-        int n=3;
-        int stars;
-        for(int i=1;i<=2*n-1;i++){
-            stars=i;
-            if(stars>n){
-                stars=2*n-i;
+        //right triangle
+        int n=6;
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
-            for(int j=1;j<=stars;j++){
-                System.out.print("*"+" ");
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
+
     }
 }
